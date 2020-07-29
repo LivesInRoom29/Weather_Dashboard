33225f8a0bb9bb4f597ac360b60aca2d21bdaf7a
LIR29 <61219066+LivesInRoom29@users.noreply.github.com>
2020-07-28 20:51:34 -0400

    Added event listener for search button to query the OpenWeatherMap API

--- Assets/script.js
+++ Assets/script.js
@@ -1,3 +1,31 @@
 $(document).ready(function() {
+    const inputEl = $('input#city-search');
+    const searchBtn = $('button.search');
+    const cityDateEl = $('h2.city-date');
 
+    // function getWeatherIcon(weather) {
+    //     const hour = moment().hour();
+    //     const sunrise = moment.unix(weather.)
+    //     if (weather === 'clear sky') {
+    //         if (hour > 5 && hour < )
+    //     }
+    // }
+
+    function displayCurrentWeather(weatherObject) {
+        const currentDate = moment().format('M/D/YYYY')
+        cityDateEl.text(`${weatherObject.name} (${currentDate})`)
+    }
+
+    searchBtn.on("click", function() {
+        event.preventDefault();
+        const zipcode = inputEl.val()
+
+        $.get(`https://api.openweathermap.org/data/2.5/weather?zip=${zipcode}&appid=e21d1a963abbd9effdb612578581c76c`)
+        .then(function(response) {
+
+            console.log(response)
+            console.log(response.name);
+            displayCurrentWeather(response)
+        });
+    })
 });
